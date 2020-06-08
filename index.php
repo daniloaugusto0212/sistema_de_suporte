7cf6b885638114b91427dc2afc0dc2ed70b90d48
daniloaugusto0212 <daniloaugusto0212@hotmail.com>
2020-06-08 20:06:05 -0300

    conectando ao BD

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+    define('HOST', 'localhost');
+    define('DATABASE','suporte_personalizado');
+    define('USER','root');
+    define('PASSWORD','681015');
+
+    $autoload = function($class){
+        include($class.'php');
+    };
+
+    spl_autoload_register($autoload);
+
+    $pdo = \MySql::conectar();
+
+?>
